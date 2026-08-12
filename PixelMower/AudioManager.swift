fed6204bfc559c3fb891ac5ac532ee15026ca309
changzhenzhong <3506104395@qq.com>
2026-08-13 01:49:36 +0800

    Update AudioManager.swift

--- PixelMower/AudioManager.swift
+++ PixelMower/AudioManager.swift
@@ -8,4 +8,6 @@ struct AudioManager {
     static func playCoin() { AudioServicesPlaySystemSound(1104) }
     static func playPickup() { AudioServicesPlaySystemSound(1105) }
     static func playDrop() { AudioServicesPlaySystemSound(1106) }
+    static func playUnlock() { AudioServicesPlaySystemSound(1110) }
+    static func playBuild() { AudioServicesPlaySystemSound(1107) }
 }
