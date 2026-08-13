d73c833236001a1125fba50197a19ea812903459
changzhenzhong <3506104395@qq.com>
2026-08-13 11:40:06 +0800

    Update App.swift

--- PixelMower/App.swift
+++ PixelMower/App.swift
@@ -1,26 +1,10 @@
 import SwiftUI
 
-// 解决竖屏锁定，替代UIApplication.shared报错
-struct OrientationLock: UIViewControllerRepresentable {
-    func makeUIViewController(context: Context) -> UIViewController {
-        let vc = UIViewController()
-        vc.view.backgroundColor = .clear
-        return vc
-    }
-    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
-        // 强制竖屏
-        uiViewController.setNeedsUpdateOfSupportedInterfaceOrientations()
-    }
-}
-
 @main
 struct PixelMowerApp: App {
     var body: some Scene {
         WindowGroup {
-            ZStack {
-                ContentView()
-                OrientationLock()
-            }
+            ContentView()
         }
     }
 }
